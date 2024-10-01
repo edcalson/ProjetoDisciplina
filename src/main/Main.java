2fc3e7aa8295e10becc4227c02986c5a9b1ffc19
edcalson <edacir@gmail.com>
2024-10-01 00:02:32 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,15 @@ package main;
 import modelo.Financiamento;
 import util.InterfaceUsuario;
 
+import java.net.FileNameMap;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
+        List<Financiamento> listadeFinanciamento = new ArrayList<>();
+
 
         double taxaJuros = interfaceUsuario.pedirTaxaJuros();
         int prazoFinanciamentoEmAnos = interfaceUsuario.pedirPrazoFinanciamento();

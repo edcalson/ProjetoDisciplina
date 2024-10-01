cb895a0d1b0dfa27bc220c0b086e00206cdb7bf2
edcalson <edacir@gmail.com>
2024-10-01 00:02:44 -0300

    Create Apartamento.java

---
+++ src/modelo/Apartamento.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Apartamento extends Financiamento {
+    public Apartamento(double valorDesejadoImovel, int prazoFinaciamentoAnos, double taxaJurosAnual) {
+        super(valorDesejadoImovel, prazoFinaciamentoAnos, taxaJurosAnual);
+    }
+
+
+    public double calcularPagamentoMensal() {
+        //
+
+        return 0;
+    }
+}
